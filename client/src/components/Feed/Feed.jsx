3ac752ef3c7f95c3c61a73bd1db9890f5cafed52
Pariwesh <tamrpariwesh@gmail.com>
2022-04-10 07:27:17 +1000

    added feed section

--- client/src/components/Feed/Feed.jsx
+++ client/src/components/Feed/Feed.jsx
@@ -1,7 +1,16 @@
+import Post from '../Post/Post'
+import Share from '../Share/Share'
 import './feed.css'
 
 const Feed = () => {
-  return <div className="feed">Feed</div>
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+        <Post />
+      </div>
+    </div>
+  )
 }
 
 export default Feed
---
+++ client/src/components/Post/Post.jsx
@@ -0,0 +1,40 @@
+import './post.css'
+import { MoreVert } from '@mui/icons-material'
+const Post = () => {
+  return (
+    <div className="post">
+      <div className="postWrapper">
+        <div className="postTop">
+          <div className="postTopLeft">
+            <img
+              className="postProfileImg"
+              src="/assets/person/1.jpeg"
+              alt=""
+            />
+            <span className="postUsername">Pariwesh Tamrakar</span>
+            <span className="postDate">5 mins ago</span>
+          </div>
+          <div className="postTopRight">
+            <MoreVert />
+          </div>
+        </div>
+        <div className="postCenter">
+          <span className="postText">Hey! it's my first post</span>
+          <img className="postImg" src="/assets/post/1.jpeg" alt="" />
+        </div>
+        <div className="postBottom">
+          <div className="postBottomLeft">
+            <img className="likeIcon" src="/assets/like.png" alt="" />
+            <img className="likeIcon" src="/assets/heart.png" alt="" />
+            <span className="postLikeCounter">7 people like it</span>
+          </div>
+          <div className="postBottomRight">
+            <span className="postCommentText">7 comments</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Post
