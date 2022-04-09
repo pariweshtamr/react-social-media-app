5ffdb45be8b12773c542b82e7f2acd4302f26d85
Pariwesh <tamrpariwesh@gmail.com>
2022-04-10 07:43:54 +1000

    added right bar

--- client/src/components/Rightbar/Rightbar.jsx
+++ client/src/components/Rightbar/Rightbar.jsx
@@ -1,7 +1,34 @@
 import './rightbar.css'
 
 const Rightbar = () => {
-  return <div className="rightbar">Rightbar</div>
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Divyesh Shrestha</b> and <b>2 other friends</b> have birthday
+            today
+          </span>
+        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgContainer">
+              <img
+                className="rightbarProfileImg"
+                src="/assets/person/3.jpeg"
+                alt=""
+              />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Elyssa Carter</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  )
 }
 
 export default Rightbar
